3144849dca34131ef78a5114090c0029cb28012a
Muhammad Touqeer <31.tmouqeer@gmail.com>
2024-09-25 05:53:03 -0700

    Star pattrent

---
+++ app.js
@@ -0,0 +1,155 @@
+var userValue =prompt("Enter A first Number for triangal")
+
+for(i=1; i<=5 ; i++)
+{
+    for (j=5; j>i; j--)
+    {
+        document.write(" &nbsp; ");
+    }
+    for(k=1 ; k<=((2*i)-1); k++)
+    {
+        document.write("*");
+    }
+    document.write("<br>");
+}
+
+
+document.write("<br>");
+
+
+// triangal pattren End
+
+
+
+// Reversed triangal pattaren start
+var userValue=prompt("Enter a second number")
+for(i=5; i>=1; i--)
+{
+    for(j=5;j>i; j--)
+        {
+            document.write(" &nbsp ");
+        }
+        for (k=1; k<=((2*i)-1);k++)
+        {
+            document.write("*");
+        }
+    document.write("<br>");
+}
+
+
+
+// Reversed triangal pattaren End
+
+
+
+// Diamond pattren is Start
+document.write("<br>")
+
+
+var userValue=prompt("Enter a third number")
+
+
+for(i=1; i<=7 ; i++)
+    {
+        for (j=7; j>i; j--)
+        {
+            document.write(" &nbsp; ");
+        }
+        for(k=1 ; k<=((2*i)-1); k++)
+        {
+            document.write("*");
+        }
+        document.write("<br>");
+    }
+
+    for(i=7; i>=1; i--)
+        {
+            for(j=7;j>i; j--)
+                {
+                    document.write(" &nbsp ");
+                }
+                for (k=1; k<=((2*i)-1);k++)
+                {
+                    document.write("*");
+                }
+            document.write("<br>");
+        }
+
+
+        document.write("<br>")
+
+        // / Diamond pattren is End
+
+
+
+        
+ var userValue=prompt("enter afour num")
+
+
+ for(i=5; i>=1; i--)
+    {
+        for(j=5;j>i; j--)
+            {
+                document.write(" &nbsp ");
+            }
+            for (k=1; k<=((2*i)-1);k++)
+            {
+                document.write("*");
+            }
+        document.write("<br>");
+    }
+
+    
+for(i=1; i<=5 ; i++)
+    {
+        for (j=5; j>i; j--)
+        {
+            document.write(" &nbsp; ");
+        }
+        for(k=1 ; k<=((2*i)-1); k++)
+        {
+            document.write("*");
+        }
+        document.write("<br>");
+    }
+
+
+document.write("<br>")  
+
+
+
+
+// for(let i = 1; i<10; i = i+2){
+    
+//      for(let k = 9; k>=i; k--){
+//          document.write("&nbsp;") 
+//      }
+
+//     for(let j = 1; j<=i; j++){
+//         document.write("*") 
+       
+//     }
+//     document.write("<br>") 
+//  }
+
+// var  userValue =prompt("enter a num")
+// main()
+// {
+
+//     let i,j,k;
+//     for(let i=1;i<=5;i++)
+//     {
+//         for(let j=5;j>i;j--)
+//         {
+//            document.write(" ") // printf(" ");
+//         }
+//         for(let k=1;k<=i;k++)
+//         {
+//            document.write("* ") // printf(" * ")
+//         }
+//         printf("\n")
+//     }
+// }
+ 
+
+   
